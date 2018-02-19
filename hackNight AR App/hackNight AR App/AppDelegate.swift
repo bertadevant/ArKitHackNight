47275e1084494ebaee384bf990c2f580132f29e5
Berta Devant <berta@novoda.com>
2018-02-19 20:41:53 +0100

    add project and assets

---
+++ hackNight AR App/hackNight AR App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  hackNight AR App
+//
+//  Created by Berta Devant on 19/02/2018.
+//  Copyright © 2018 Berta Devant. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
